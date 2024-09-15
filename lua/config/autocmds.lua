eb9c88d974c012e922e4158c12c04ea98a7d6fa6
Hoang Hoa <44923253+hoanghoa12345@users.noreply.github.com>
2024-09-15 20:35:29 +0700

    feat: add autocmds to format file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,31 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+local function is_deno_lsp()
+  local clients = vim.lsp.get_active_clients()
+  for _, client in ipairs(clients) do
+    if client.name == "denols" and client.server_capabilities.documentFormattingProvider then
+      return true
+    end
+  end
+  return false
+end
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.ts", "*.js", "*.tsx", "*.jsx" },
+  desc = "Auto-format Deno files after saving",
+  callback = function()
+    local fileName = vim.api.nvim_buf_get_name(0)
+    if is_deno_lsp() then
+      vim.cmd("silent !deno fmt " .. fileName)
+    else
+      vim.cmd("silent !prettier --write " .. fileName)
+    end
+  end,
+  group = augroup("deno_formatting"),
+})
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,23 +1,13 @@
 return {
-  {
-    "lukas-reineke/lsp-format.nvim",
-    config = function()
-      require("lsp-format").setup({})
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local nvim_lsp = require("lspconfig")
-      nvim_lsp.denols.setup({
-        on_attach = require("lsp-format").on_attach,
-        root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
-      })
-      nvim_lsp.tsserver.setup({
-        on_attach = require("lsp-format").on_attach,
-        root_dir = nvim_lsp.util.root_pattern("package.json"),
-        single_file_support = false,
-      })
-    end,
-  },
+  "neovim/nvim-lspconfig",
+  config = function()
+    local nvim_lsp = require("lspconfig")
+    nvim_lsp.denols.setup({
+      root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+    })
+    nvim_lsp.tsserver.setup({
+      root_dir = nvim_lsp.util.root_pattern("package.json"),
+      single_file_support = false,
+    })
+  end,
 }
